87999a323d722014cd79be2c5859b14f06c24218
Alejandro Leonian <66581753+aleleonian@users.noreply.github.com>
2024-02-14 12:36:22 -0500

    Implemented put endpoint

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,53 +1,67 @@
-const blogRouter = require('express').Router();
-const Blog = require('../models/blog');
-const mongoose = require('mongoose');
+const blogRouter = require("express").Router();
+const Blog = require("../models/blog");
+const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
 
-blogRouter.get('/api/blogs', async (request, response) => {
-    const blogPosts = await Blog.find({});
-    response.json(blogPosts);
-})
+blogRouter.get("/api/blogs", async (request, response) => {
+  const blogPosts = await Blog.find({});
+  response.json(blogPosts);
+});
 
-blogRouter.post('/api/blogs', async (request, response) => {
-    const blog = new Blog(request.body);
+blogRouter.post("/api/blogs", async (request, response) => {
+  const blog = new Blog(request.body);
 
-    if (!blog.title || !blog.url) return response.status(400).end();
+  if (!blog.title || !blog.url) return response.status(400).end();
 
-    if (!blog.likes) blog.likes = 0;
+  if (!blog.likes) blog.likes = 0;
 
-    const result = await blog.save();
+  const result = await blog.save();
 
-    response.status(201).json(result);
-})
+  response.status(201).json(result);
+});
 
-blogRouter.delete('/api/blogs/:id', async (request, response) => {
+blogRouter.delete("/api/blogs/:id", async (request, response) => {
+  const isValidObjectId = mongoose.Types.ObjectId.isValid(request.params.id);
 
-    const isValidObjectId = mongoose.Types.ObjectId.isValid(request.params.id);
+  let objectId;
 
-    let objectId;
+  if (isValidObjectId) {
+    objectId = new ObjectId(request.params.id);
+  } else {
+    console.error("Invalid ObjectId format");
+    return response.status(400).end();
+  }
 
-    if (isValidObjectId) {
-        objectId = new ObjectId(request.params.id);
+  try {
+    const deletedDocument = await Blog.findByIdAndDelete(objectId);
 
+    if (deletedDocument) {
+      console.log("Document deleted successfully:", deletedDocument);
     } else {
-        console.error('Invalid ObjectId format');
-        return response.status(400).end();
+      console.log("Document not found");
     }
+  } catch (error) {
+    console.error("Error deleting document:", error);
+  }
 
-    try {
-        const deletedDocument = await Blog.findByIdAndDelete(objectId);
+  response.status(204).end();
+});
 
-        if (deletedDocument) {
-            console.log('Document deleted successfully:', deletedDocument);
-        } else {
-            console.log('Document not found');
-        }
-    }
-    catch (error) {
-        console.error('Error deleting document:', error);
-    }
+blogRouter.put("/api/blogs/:id", (request, response, next) => {
+    
+  const body = request.body;
 
-    response.status(204).end()
-})
+  const blogPost = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
 
+  Blog.findByIdAndUpdate(request.params.id, blogPost, { new: true })
+    .then((updatedBlogpost) => {
+      response.json(updatedBlogpost);
+    })
+    .catch((error) => next(error));
+});
 module.exports = blogRouter;
