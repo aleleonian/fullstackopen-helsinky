3880cd2c7094477a3137803098cfb7cd8197d2b0
Alejandro Leonian <66581753+aleleonian@users.noreply.github.com>
2024-02-14 17:15:08 -0500

    4-15 ready

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -1,11 +1,25 @@
 const bcrypt = require('bcryptjs');
+
 const usersRouter = require('express').Router();
+
 const User = require('../models/user');
 
-usersRouter.post('/', async (request, response) => {
+usersRouter.get('/api/users', async (request, response) => {
+  try {
+    const allUsers = (await User.find({})).map((user) => user.toJSON());
+    response.json(allUsers);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log('error->', error);
+    response.status(500).json(error);
+  }
+});
+
+usersRouter.post('/api/users', async (request, response) => {
   const { username, name, password } = request.body;
 
   const saltRounds = 10;
+
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
   const user = new User({
