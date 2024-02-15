f28c7844cb24e5daa15c67030e0e2dc168d666ec
Alejandro Leonian <alejandromleonian@gmail.com>
2024-02-14 19:41:25 -0500

    invalid user tests created

--- part4/bloglist/__test__/users-api.test.js
+++ part4/bloglist/__test__/users-api.test.js
@@ -23,6 +23,69 @@ describe('Invalid user creation', () => {
 
     expect(response.body.error).toBe('Password must be at least 7 chars long.');
   });
+
+  test('Empty username', async () => {
+    const newUser = {
+      username: '',
+      name: 'thenameoftheperson',
+      password: 'thelongpassword',
+    };
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body.message).toMatch(/`username` is required./);
+  });
+
+  test('Short username', async () => {
+    const newUser = {
+      username: 'he',
+      name: 'thenameoftheperson',
+      password: 'thelongpassword',
+    };
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body.message).toMatch(/is shorter than the minimum allowed length/);
+  });
+  test('Short name', async () => {
+    const newUser = {
+      username: 'theusername',
+      name: 'aa',
+      password: 'thelongpassword',
+    };
+
+    const response = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(response.body.message).toMatch(/name: Path `name` \((.*?)\) is shorter than the minimum allowed length \(\d+\)\.$/);
+  });
+});
+
+test('Empty name', async () => {
+  const newUser = {
+    username: 'theusername',
+    name: '',
+    password: 'thelongpassword',
+  };
+
+  const response = await api
+    .post('/api/users')
+    .send(newUser)
+    .expect(400)
+    .expect('Content-Type', /application\/json/);
+
+  expect(response.body.message).toMatch(/`name` is required./);
 });
 
 afterAll(async () => {
