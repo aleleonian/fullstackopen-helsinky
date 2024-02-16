00c78177e51c67606dfeb310f51da8f7cc51f278
Alejandro Leonian <66581753+aleleonian@users.noreply.github.com>
2024-02-16 17:21:42 -0500

    working on part2 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,28 +1,50 @@
 import { useState } from 'react'
 
-const Button = ({ title, onClickHandler, counter, counterFunction }) => {
+const Button = ({ title, onClickHandler }) => {
   return (
-    <button onClick={() => onClickHandler(counter, counterFunction)}>{title}</button>
+    <button onClick={onClickHandler}>{title}</button>
   )
 };
 
-const clickHandler = (counter, counterFunction) => {
-  let newCounter = counter + 1;
-  counterFunction(newCounter);
-}
-
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // const clickHandler = (counter, counterFunction) => {
+  //   let newCounter = counter + 1;
+  //   counterFunction(newCounter);
+  // }
+
+  const goodClickHandler = () => {
+    let goodCounter = good;
+    setGood(goodCounter + 1);
+  }
+  const neutralClickHandler = () => {
+    let neutralCounter = neutral;
+    setNeutral(neutralCounter + 1);
+  }
+  const badClickHandler = () => {
+    let badCounter = bad;
+    setBad(badCounter + 1);
+  }
+
+  const generateAverage = (good, bad, neutral) => {
+    if (good === 0 && bad === 0 && neutral === 0) return 0;
+    return ((good * 1) + (neutral * 0) + (bad * -1)) / (good + neutral + bad)
+  }
+  const generatePositive = (good, bad, neutral) => {
+    if (good === 0 && bad === 0 && neutral === 0) return 0;
+    return ((good + neutral) / (good + neutral + bad)) * 100;
+  }
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button title="good" counter={good} counterFunction={setGood} onClickHandler={clickHandler} />
-      <Button title="neutral" counter={bad} counterFunction={setNeutral} onClickHandler={clickHandler} />
-      <Button title="bad" counter={neutral} counterFunction={setBad} onClickHandler={clickHandler} />
+      <Button title="good" onClickHandler={goodClickHandler} />
+      <Button title="neutral" onClickHandler={neutralClickHandler} />
+      <Button title="bad" onClickHandler={badClickHandler} />
       <br />
       <h1> Statistics </h1>
       <div>
@@ -37,6 +59,17 @@ const App = () => {
         Bad: {bad}
       </div>
       <br />
+      <div>
+        Total: {good + neutral + bad}
+      </div>
+      <br />
+      <div>
+        Average: {generateAverage(good, neutral, bad)}
+      </div>
+      <br />
+      <div>
+        Positive: {generatePositive(good, neutral, bad)}%
+      </div>
     </div>
   )
 }
