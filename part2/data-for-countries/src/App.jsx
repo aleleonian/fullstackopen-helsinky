8eb89167b113277cf0a7de46221666ac3451c2f7
Alejandro Leonian <66581753+aleleonian@users.noreply.github.com>
2024-02-20 17:41:41 -0500

    working on it

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -2,7 +2,21 @@ import axios from 'axios';
 import { useState, useEffect } from 'react'
 import './App.css'
 
+const FilteredResults = ({ results }) => {
 
+  if (results.length > 10) return "Too many matches, please specify another filter."
+  else {
+    return results.map((country, index) => {
+      return (
+      <div key={country}>
+        {country}
+        {/* <button onClick={() => { deleteHandler(person.id, person.name) }}>delete</button> */}
+        {/* {index !== results.length - 1 && <br />}{" "} */}
+      </div>)
+    })
+  }
+
+}
 
 function App() {
 
@@ -22,32 +36,20 @@ function App() {
   }
 
   const filterResults = () => {
-    const filteredResults =
-      countries.
-        filter((country) => {
-          if (country.name.common.toLowerCase().indexOf(searchFilter) > -1) return true;
-        })
-        .map(country => country.name.common);
-
-    if (filteredResults.length > 10) return "Too many matches, please specify another filter."
-    else {
-      return filteredResults
-        // .map((country, index) => (
-        //   <div key={country.name}>
-        //     {country.name}
-        //     {/* <button onClick={() => { deleteHandler(person.id, person.name) }}>delete</button> */}
-        //     {index !== filteredResults.length - 1 && <br />}{" "}
-        //   </div>
-        // ))
-    }
+    return countries.
+      filter((country) => {
+        if (country.name.common.toLowerCase().indexOf(searchFilter) > -1) return true;
+      })
+      .map(country => country.name.common);
   }
+
   return (
     <>
       <div>
         find country:<input type="text" id="countryInput" onChange={handleInput} />
       </div>
       {
-        searchFilter.length > 0 ? filterResults()
+        searchFilter.length > 0 ? <FilteredResults results={filterResults()} />
           :
           ""
       }
