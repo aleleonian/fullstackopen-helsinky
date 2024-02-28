5ca73f57a5e9496e74b5f79ead827b6d5c9e62fa
Alejandro Leonian <alejandromleonian@gmail.com>
2024-02-28 09:18:31 -0500

    Starting to work on the frontend for the gbloglist backend

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,23 +1,81 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
+const handleLogin = async (event) => {
+  event.preventDefault()
+
+  try {
+    const user = await loginService.login({
+      username, password,
+    })
+    setUser(user)
+    setUsername('')
+    setPassword('')
+  } catch (exception) {
+    setErrorMessage('Wrong credentials')
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+  // useEffect(() => {
+  //   blogService.getAll().then(blogs =>
+  //     setBlogs(blogs)
+  //   )
+  // }, [])
+
+  const loginForm = () => (
+    <>
+      <h1>Login to application</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
+  )
+
+  const bloglistForm = () => {
+    return (
+      <div>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
 
   return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+    <>
+      {user && bloglistForm()}
+      {!user && loginForm()}
+    </>
   )
 }
 
