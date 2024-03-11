b850b24ec2d82055c0518002fddb72b087b88fe8
Alejandro Leonian <alejandromleonian@gmail.com>
2024-03-11 17:55:41 -0500

    view/hide button implemented

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title}:{blog.author}
-  </div>  
-)
+import { useState } from "react";
+
+
+const Blog = ({ blog }) => {
+  const [displayInfo, setDisplayInfo] = useState(false);
+
+  const toggleShowInfo = () => {
+    setDisplayInfo(!displayInfo);
+  }
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} <button onClick={toggleShowInfo}>{!displayInfo ? "view" : "hide"}</button>
+      {displayInfo &&
+        <>
+          <div>{blog.url}</div>
+          <div>{blog.likes} <button>like</button></div>
+          <div>{blog.author}</div>
+        </>
+      }
+    </div>
+  )
+}
+
 
 export default Blog
