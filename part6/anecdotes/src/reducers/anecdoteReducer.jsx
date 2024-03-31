93dcb5855b2eb07fc5a2d665e7d2504948693a0d
Alejandro Leonian <alejandromleonian@gmail.com>
2024-03-31 10:24:04 -0500

    removed a couple of files and updated others

--- part6/anecdotes/src/reducers/anecdoteReducer.jsx
+++ part6/anecdotes/src/reducers/anecdoteReducer.jsx
@@ -6,31 +6,6 @@ const initialState = {
   selectedAnecdoteIndex: 0,
 };
 
-// export const createAnecdote = (anecdote) => {
-//   return {
-//     type: "NEW_ANECDOTE",
-//     payload: anecdote,
-//   };
-// };
-
-// export const anecdoteReducer = (state = initialState, action) => {
-//   let newState = { ...state };
-
-//   switch (action.type) {
-//     case "VOTE":
-//       newState.anecdotes[newState.selectedAnecdoteIndex].votes++;
-//       return newState;
-//     case "NEW_ANECDOTE":
-//       newState.anecdotes.push({ string: action.payload, votes: 0 });
-//       return newState;
-//     case "SELECT":
-//       newState.selectedAnecdoteIndex = action.payload;
-//       return newState;
-//     default:
-//       return newState;
-//   }
-// };
-
 const anecdotesSlice = createSlice({
   name: "anecdotes",
   initialState,
