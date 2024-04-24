f23fd2e36c175b6a5ea6ee37b172a41133b1367a
Alejandro Leonian <66581753+aleleonian@users.noreply.github.com>
2024-04-24 17:27:12 -0500

    it works, there's a bug tho. State keeps being set

---
+++ part7/country-hook/src/hooks/index.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    return {
+        type,
+        value,
+        onChange
+    }
+}
+
+export const useCountry = (name) => {
+    const [country, setCountry] = useState(null)
+    useEffect(() => {
+        debugger;
+        if (name != "") {
+            axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+                .then(response => {
+                    const countryData = {};
+                    countryData.data = response.data;
+                    countryData.found = true;
+                    setCountry(countryData);
+                })
+                .catch(error => {
+                    setCountry({ found: false });
+                })
+        }
+    }, [country])
+
+    return country
+}
+
